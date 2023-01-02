74bffbf43419b8033d33938d074142720aa2067a
Nathan <79526162+Nathan-FiveM@users.noreply.github.com>
2023-01-02 12:33:53 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,13 +5,13 @@ author 'Nathan#8860'
 description 'QB Roulette'
 version '1.0.0'
 
-client_scripts {	
-	'client/main.lua',
+client_scripts {    
+    'client.lua',
 }
 
 ui_page 'index.html'
 file 'index.html'
 
-server_scripts {	
-	'server/main.lua',
+server_scripts {    
+    'server.lua',
 }
